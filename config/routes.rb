3e3b77ec1664bb6b0b4fad0926e5d24ab5415c6a
Jon Waterman <JonRWaterman@gmail.com>
2022-01-05 11:34:52 +0200

    file restructure v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  
-  get "/articles", to: "articles#index"
-
-  get "/student", to: "articles#student"
-
-  get "/admin", to: "articles#admin"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
