e56281a3d84e2a8dc92f3f0c6694d07756297bf1
Kareem Hirani <kareem@DESKTOP-MNTAG04.localdomain>
2022-01-06 20:10:29 +0200

    cruds for group and game implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  resources :groups
   
   get "/articles", to: "articles#index"
 
   get "/student", to: "articles#student"
 
   get "/admin", to: "articles#admin"
+
+  get "/groups", to: "groups#_group"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :games
+    root :to => redirect('/games')
+
+    
 end
--- db/migrate/20220106151749_create_games.rb
+++ db/migrate/20220106151749_create_games.rb
@@ -1,9 +1,10 @@
 class CreateGames < ActiveRecord::Migration[7.0]
   def change
-    create_table :games, :id => false do |t|
-      t.integer :id
+    create_table :games do |t|
+      t.string :hometeam
       t.string :opponent
-      t.timestamp :date
+      t.datetime :gamedate
+      t.string :day
 
       t.timestamps
     end
