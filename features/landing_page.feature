f8cef070b005478161a2a93b4fea5a26c238853b
reidneason <reidneason@yahoo.com>
2022-01-06 21:27:04 +0200

    Create paths.rb

---
+++ features/landing_page.feature
@@ -0,0 +1,6 @@
+Feature: have a functional landing page
+
+  As a user
+  So that I can access the rest of the website
+  I want a login page with times for upcoming games and pulls
+
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,1 +1,14 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(locator) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
