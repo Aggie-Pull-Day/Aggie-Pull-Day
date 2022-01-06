1fbcfc0d0eadd057495a52213437f0da35f5e077
reidneason <reidneason@yahoo.com>
2022-01-06 22:04:26 +0200

    Home page tests

--- features/landing_page.feature
+++ features/landing_page.feature
@@ -4,3 +4,14 @@ Feature: have a functional landing page
   So that I can access the rest of the website
   I want a login page with times for upcoming games and pulls
 
+Scenario: functioning student login link
+
+  Given I am on the home page
+  When I press "Student Login"
+  Then I should be on the student login page
+
+Scenario: functioning admin login link
+
+    Given I am on the home page
+    When I press "Admin Login"
+    Then I should be on the admin login page
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,14 +1,29 @@
-require 'uri'
-require 'cgi'
+# frozen_string_literal: true
+
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
 
 module WithinHelpers
-  def with_scope(locator)
-    locator ? within(locator) { yield } : yield
+  def with_scope(locator, &block)
+    locator ? within(locator, &block) : yield
   end
 end
 World(WithinHelpers)
 
-Given /^(?:|I )am on (.+)$/ do |page_name|
+Given /^(?:|I )am on the (.+)$/ do |page_name|
   visit path_to(page_name)
+end
+
+When /^(?:|I )press "([^\"]*)"(?: within "([^\"]*)")?$/ do |button, selector|
+  with_scope(selector) do
+    click_button(button)
+  end
+end
+
+Then /^(?:|I )should be on the (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
 end
