a6aba8605364375230249139f700ca2bfa64c25a
Kareem Hirani <kareem@DESKTOP-MNTAG04.localdomain>
2022-01-06 20:45:31 +0200

    added custom 404 page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   resources :groups
   
   get "/articles", to: "articles#index"
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
 
   get "/groups", to: "groups#_group"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  match "/404", to: "errors#not_found", via: :all
   # Defines the root path route ("/")
   # root "articles#index"
   resources :games
