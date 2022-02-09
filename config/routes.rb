c0ba937b271fcae220d44c0473ece4fca22416f7
reidneason <reidneason@yahoo.com>
2022-02-09 00:42:55 -0600

    More merge conflicts solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'errors/not_found'
   get 'errors/internal_server_error'
   resources :groups
+  
+  get "/articles", to: "articles#index"
 
-  get '/articles', to: 'articles#index'
+  get "/student", to: "articles#student"
 
-  get '/student', to: 'articles#student'
+  get "/admin", to: "articles#admin"
 
-  get '/admin', to: 'articles#admin'
-
-  get '/groups', to: 'groups#_group'
+  get "/groups", to: "groups#_group"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  match '/404', to: 'errors#not_found', via: :all
+  match "/404", to: "errors#not_found", via: :all
   # Defines the root path route ("/")
   # root "articles#index"
   resources :games
-  root to: redirect('/games')
+    root :to => redirect('/games')
 
+    
 end
