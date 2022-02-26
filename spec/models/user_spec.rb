440c49263d9e9ec82127606de9cc11ef17244442
reidneason <reidneason@yahoo.com>
2022-02-26 13:15:38 -0600

    Added User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,27 +12,27 @@ RSpec.describe User, type: :model do
     User.create(email: 'JonW@tamu.edu', password_digest: 'Dummy') if User.where(email: 'JonW@tamu.edu').empty?
   end
 
-  describe 'model' do
-    xit 'has the correct amount of data' do
+  describe 'model', pending: true do
+    it 'has the correct amount of data' do
       expect(User.all.length).to eq 4
     end
 
-    xit 'loads users based on a single attribute' do
+    it 'loads users based on a single attribute' do
       unencrypted = User.where(password_digest: 'Dummy')
       expect(unencrypted.length).to eq 3
     end
 
-    xit 'loads users based on multiple attributes' do
+    it 'loads users based on multiple attributes' do
       reid_unencrypted = User.where(email: 'ReidN@tamu.edu', password_digest: false)
       expect(reid_unencrypted.length).to eq 1
     end
 
-    xit 'deletes users based on a single attribute' do
+    it 'deletes users based on a single attribute' do
       Users.where(email: 'ReidN@tamu.edu').destroy_all
       expect(User.all.length).to eq 3
     end
 
-    xit 'deletes users based on multiple attributes' do
+    it 'deletes users based on multiple attributes' do
       Users.where(email: 'BaldwinB@tamu.edu', password_digest: 'Dummy').destroy_all
       expect User.all.length to eq 2
     end
@@ -43,21 +43,41 @@ RSpec.describe User, type: :model do
       User.create(email: 'PhilipR@tamu.edu', password: 'OldArmy')
     end
 
-    xit 'returns the proper message' do
+    it 'returns the proper message' do
       expect(user.welcome).to eq 'Hello, PhilipR@tamu.edu!'
     end
   end
 
-  describe 'hasPulled' do
+  describe 'hasPulled', pending: true do
     let(:user) do
       User.create(email: 'PhilipR@tamu.edu', password: 'OldArmy')
     end
+    let(:group) do
+      Group.create(groupname: 'The Professors', member: 'Philip Ritchey', pulled: false, email: 'PhilipR@tamu.edu')
+      Group.create(groupname: 'The Professors', member: 'Robert Lightfoot', pulled: false, email: 'RobertL@tamu.edu')
+    end
+
+    it 'returns the proper status' do
+      expect(user.hasPulled).to eq false
+    end
   end
 
-  describe 'getTeam' do
+  describe 'getTeam', pending: true do
   end
 
-  describe 'Pull' do
+  describe 'Pull', pending: true do
+    let(:user) do
+      User.create(email: 'RobertL@tamu.edu', password: 'OldArmy')
+    end
+    let(:group) do
+      Group.create(groupname: 'The Professors', member: 'Philip Ritchey', pulled: false, email: 'PhilipR@tamu.edu')
+      Group.create(groupname: 'The Professors', member: 'Robert Lightfoot', pulled: false, email: 'RobertL@tamu.edu')
+    end
+
+    it 'pulls for the group member' do
+      user.Pull
+      expect(Group.where(email: user.email).pulled).to eq true
+    end
   end
 end
 
