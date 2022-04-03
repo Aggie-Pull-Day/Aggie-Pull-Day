090f0ebee1480d7279654f89e923ed7b65e1b3e0
reidneason <reidneason@yahoo.com>
2022-04-03 14:19:08 -0500

    Create pullDate function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,58 +1,81 @@
 class User < ApplicationRecord
-    belongs_to :group, optional: true
-    has_one :seat
-    has_secure_password
+  belongs_to :group, optional: true
+  has_one :seat
+  has_secure_password
 
-    def welcome
-        "Hello, #{self.email}!"
+  def welcome
+    "Hello, #{email}!"
+  end
+
+  def hasPulled
+    res = Group.where(email: email)
+    # results = ActiveRecord::Base.connection.execute("select groups.pulled from groups INNER JOIN users ON groups.email = '#{self.email}';")
+    res.first.pulled
+  end
+
+  def pullTime
+    this_group = Group.where(id: groupID).first
+    group_members = User.where(groupID: this_group['groupID'])
+    group_size = group_members.length
+    num_grads = group_members.where(classification: 'U5').length
+    num_seniors = group_members.where(classification: 'U4').length
+    num_juniors = group_members.where(classification: 'U3').length
+    num_sophomores = group_members.where(classification: 'U2').length
+
+    this_game = Game.where(id: this_group.gameID).first
+    gamedate = this_game['gamedate']
+
+    pulldate = if num_grads + num_seniors >= group_size / 2.0
+                 gamedate - 432_000
+               elsif num_juniors >= group_size / 2.0
+                 gamedate - 345_600
+               elsif num_sophomores >= group_size / 2.0
+                 gamedate - 259_200
+               else
+                 gamedate - 172_800
+               end
+    Time.new(pulldate.year, pulldate.month, pulldate.day, 0, 0, 0)
+  end
+
+  def getTeam
+    res = Group.where(email: email)
+    # results = ActiveRecord::Base.connection.execute("select groups.groupname from groups INNER JOIN users ON groups.email = '#{self.email}';")
+    # puts results
+    if res.empty?
+      return []
     end
 
-    def hasPulled
+    team = res.first['groupname']
 
-        res = Group.where(email: self.email)
-        # results = ActiveRecord::Base.connection.execute("select groups.pulled from groups INNER JOIN users ON groups.email = '#{self.email}';")
-        return res.first.pulled
+    if team.blank?
+      return []
     end
 
-    def getTeam
-        res = Group.where(email: self.email)
-        # results = ActiveRecord::Base.connection.execute("select groups.groupname from groups INNER JOIN users ON groups.email = '#{self.email}';")
-        # puts results
-        if res.empty?
-            return []
-        end
+    members = Group.where(groupname: team)
+    return members
+  end
 
-        team = res.first["groupname"]
+  def Pull
+    res = Group.where(email: email)
+    team = res.first['groupname']
+    members = Group.where(groupname: team)
 
-        if team.blank?
-            return []
-        end
+    ticketsToPull = members.length
 
-        members = Group.where(groupname: team)
-        return members
+    availableSeats = Seat.where(assigned: false)
+
+    iterate = 0
+
+    members.each do |member|
+
+      member.update_attribute(:seatnumber, availableSeats[iterate].seatnumber)
+      member.update_attribute(:pulled, true)
+      availableSeats[iterate].update_attribute(:assigned, true)
+      availableSeats[iterate].update_attribute(:email, member.email)
+
+      iterate = iterate + 1
     end
 
-    def Pull
-        res = Group.where(email: self.email)
-        team = res.first["groupname"]
-        members = Group.where(groupname: team)
-
-        ticketsToPull = members.length
-
-        availableSeats = Seat.where(assigned: false)
-
-        iterate = 0
-
-        members.each do |member|
-
-            member.update_attribute(:seatnumber, availableSeats[iterate].seatnumber)
-            member.update_attribute(:pulled, true)
-            availableSeats[iterate].update_attribute(:assigned, true)
-            availableSeats[iterate].update_attribute(:email, member.email)
-
-            iterate = iterate + 1
-        end
-
-    end
+  end
 
 end
