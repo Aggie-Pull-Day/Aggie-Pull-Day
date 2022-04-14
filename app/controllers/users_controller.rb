fc9a33742a7aeb910d76f963d24a79c9d481d716
Jon Waterman <JonRWaterman@gmail.com>
2022-04-14 12:10:27 -0500

    added back all CRUD for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     end
   
     def index
+      @users = User.all
     end
 
     def display
@@ -15,6 +16,11 @@ class UsersController < ApplicationController
    
     def new
       @user = User.new(:pulled => false)
+    end
+
+    # GET /users/1/edit
+    def edit
+      @user = User.find(params[:id])
     end
   
     def create
@@ -27,9 +33,42 @@ class UsersController < ApplicationController
         redirect_to new_user_path
       end
     end
+
+    # PATCH/PUT /users/1 or /users/1.json
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    # DELETE /people/1 or /people/1.json
+  def destroy
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to people_url, notice: "User was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
   
     private 
     def user_params
       params.require(:user).permit(:email, :password)
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.fetch(:user, {})
     end
 end
