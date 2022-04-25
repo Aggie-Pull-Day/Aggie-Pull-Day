7afaa755f99e91ca737697edd02a745a36ef94ae
reidneason <reidneason@yahoo.com>
2022-04-24 19:26:22 -0500

    Functioning navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,23 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login, only: [:create, :new]
 
-  def new
-  end
-
+  def new; end
 
   def create
     session_params = params.permit(:authenticity_token, :email, :password, :commit)
     @user = User.find_by(email: session_params[:email])
-    if @user && @user.authenticate(session_params[:password])
+    if @user&.authenticate(session_params[:password])
       session[:user_id] = @user.id
       redirect_to @user
     else
-      flash[:notice] = "Login is invalid!"
+      flash[:notice] = 'Login is invalid!'
       redirect_to new_session_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "You have been signed out!"
+    flash[:notice] = 'You have been signed out!'
     redirect_to new_session_path
   end
 end
