ea6cf8615e7c1b55c268621a44f471557f5e2ab5
reidneason <reidneason@yahoo.com>
2022-04-25 13:07:56 -0500

    Add admin status to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_13_001808) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_25_180243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_001808) do
     t.boolean "pulled"
     t.bigint "group_id"
     t.integer "uin"
-    t.string "classification"
+    t.boolean "admin"
     t.index ["group_id"], name: "index_users_on_group_id"
   end
 
