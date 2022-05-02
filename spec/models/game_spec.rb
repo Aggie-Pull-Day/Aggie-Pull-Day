87ef155fccc13a440112c6f64b966c989aba083f
reidneason <reidneason@yahoo.com>
2022-05-02 12:10:40 -0500

    More games

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   describe 'model' do
     it 'has the correct amount of data' do
-      expect(Game.all.length).to eq 3
+      expect(Game.all.length).to eq 17
     end
 
     it 'loads games based on a single attribute' do
@@ -21,25 +21,25 @@ RSpec.describe Game, type: :model do
     it 'updates one attribute of a game' do
       Game.first.update(opponent: 'tu')
       expect(Game.where(opponent: 'tu').length).to eq 1
-      expect(Game.where(opponent: 'Sam Houston State').length).to eq 0
+      expect(Game.where(opponent: 'Kent State').length).to eq 0
     end
 
     it 'updates multiple attributes of a game' do
       Game.first.update(opponent: 'tu', gamedate: '3-Sep-2025')
       expect(Game.where(opponent: 'tu').length).to eq 1
       expect(Game.where(gamedate: '3-Sep-2025').length).to eq 1
-      expect(Game.where(opponent: 'Sam Houston State').length).to eq 0
-      expect(Game.where(gamedate: '3-Sep-2022').length).to eq 0
+      expect(Game.where(opponent: 'Kent State').length).to eq 0
+      expect(Game.where(gamedate: '4-Sep-2021').length).to eq 0
     end
 
     it 'deletes games based on a single attribute' do
       Game.where(gamedate: '3-Sep-2022').destroy_all
-      expect(Game.all.length).to eq 2
+      expect(Game.all.length).to eq 16
     end
 
     it 'deletes games based on multiple attributes' do
       Game.where(opponent: 'Sam Houston State', gamedate: '3-Sep-2022').destroy_all
-      expect(Game.all.length).to eq 2
+      expect(Game.all.length).to eq 16
     end
   end
 end
