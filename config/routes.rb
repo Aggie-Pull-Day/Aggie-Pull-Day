9dfb053da412cb5529337584cab2cc801741f010
reidneason <reidneason@yahoo.com>
2022-05-06 19:53:40 -0500

    Make Group Owner button

--- config/routes.rb
+++ config/routes.rb
@@ -6,33 +6,34 @@ Rails.application.routes.draw do
   get 'errors/internal_server_error'
   resources :groups
 
-  get "/articles", to: "articles#index"
+  get '/articles', to: 'articles#index'
 
-  get "/student", to: "articles#student"
+  get '/student', to: 'articles#student'
 
-  get "/admin", to: "articles#admin"
+  get '/admin', to: 'articles#admin'
 
   get '/dashboard', to: 'articles#dashboard'
 
-  get "/groups", to: "groups#_group"
+  get '/groups', to: 'groups#_group'
 
   get '/users/display' => 'users#display'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  match "/404", to: "errors#not_found", via: :all
+  match '/404', to: 'errors#not_found', via: :all
   # Defines the root path route ("/")
   # root "articles#index"
   resources :games
   root to: redirect('/articles')
 
-  resources :users, only: [:new, :create, :destroy, :edit, :update, :show]
+  resources :users, only: %i[new create destroy edit update show]
   resources :users do
     post :leave_group, on: :member
     post :remove_from_group, on: :member
+    post :reassign_group_ownership, on: :member
     # get :join_group, on: :member
   end
 
   get 'users/:id/join_group', to: 'users#join_group', as: :join_group
   post 'users/:id/join_group', to: 'users#add_to_group', as: :add_to_group
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: %i[new create destroy]
 end
