9f385d4eac85195bc778fc1cb534eb6229f6a0d1
Jon Waterman <JonRWaterman@gmail.com>
2022-05-09 08:53:56 -0500

    add error checking to invite

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -7,19 +7,21 @@ class InvitesController < ApplicationController
     invitee = params[:email]
     inviter = User.find(session[:user_id]).get_email
     group_id = User.find(session[:user_id]).group_id
-    inviter_accepted = true
-    inviter_accepted = true
 
-    Invite.create(group_id: group_id, inviter: inviter, invitee: invitee, inviter_accepted: true, invitee_accepted: false)
+    invite = Invite.create(group_id: group_id, inviter: inviter, invitee: invitee, inviter_accepted: true, invitee_accepted: false)
+    if not Student.find_by(email: invitee) == nil
 
-    # call method to send an invite here
-    group = Group.find(group_id)
-    InviteMailer.with(invitee: Student.find_by(email: invitee), inviter: Student.find_by(email:inviter), group: group).email_sent.deliver_now
-    redirect_to new_invite_path
+        group = Group.find(group_id)
+        InviteMailer.with(invitee: Student.find_by(email: invitee), inviter: Student.find_by(email:inviter), group: group).email_sent.deliver_now
+        flash[:success] = 'Invite sent!'
+
+        redirect_to new_invite_path
+    else
+      flash[:error] = 'Error: please enter a valid email address.'
+      redirect_to new_invite_path
+    end
 
   end
 
-  def send_invite
-  end
 
 end
