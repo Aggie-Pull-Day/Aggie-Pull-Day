852db316a19a50a0ad0a6f2383c3aa0d991a27ac
Jon Waterman <JonRWaterman@gmail.com>
2022-05-10 01:17:22 -0500

    cleaned up tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_08_192551) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_055718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,14 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_08_192551) do
     t.string "code"
   end
 
-  create_table "pull_times", force: :cascade do |t|
-    t.integer "weekday"
-    t.time "start_time"
-    t.time "end_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-    
   create_table "invites", force: :cascade do |t|
     t.integer "group_id"
     t.string "inviter"
@@ -49,12 +41,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_08_192551) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "seats", force: :cascade do |t|
-    t.string "seatnumber"
-    t.boolean "assigned"
+  create_table "pull_times", force: :cascade do |t|
+    t.integer "weekday"
+    t.time "start_time"
+    t.time "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
   end
 
   create_table "students", force: :cascade do |t|
